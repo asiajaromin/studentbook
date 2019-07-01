e1723ce32d108097cae44d1fd0b10ff3a7bae7d9
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-01 15:09:17 +0200

    First commit

---
+++ src/main/java/pl/jcommerce/joannajaromin/studentbook/StudentbookApplication.java
@@ -0,0 +1,13 @@
+package pl.jcommerce.joannajaromin.studentbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentbookApplication.class, args);
+	}
+
+}
