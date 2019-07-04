5b691e9aed0f565718f22da69bf731d4b84d9514
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-04 14:25:30 +0200

    corrected sql scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+TRUNCATE student_book.students RESTART IDENTITY CASCADE;
+INSERT INTO student_book.students (student_id, first_name, last_name, email)
+VALUES 
+(1,'Jan', 'Kowalski', 'jan.kowalski@twojmail.com'),
+(2,'Patryk', 'Nowak', 'patryk.nowak@skrzyneczka.pl'),
+(3,'Barbara', 'Kwiatek', 'kwiatuszek@twojmail.com');
+TRUNCATE student_book.subjects RESTART IDENTITY CASCADE;
+INSERT INTO student_book.subjects (subject_id,subject_name)
+VALUES 
+(1,'Matematyka'),
+(2,'Język Polski'),
+(3,'Język Angielski'),
+(4,'Historia'),
+(5,'Biologia');
+
+TRUNCATE student_book.grades RESTART IDENTITY CASCADE;
+INSERT INTO student_book.grades (grade_id, student_id, subject_id, grade)
+VALUES
+(1,1,1,5),
+(2,1,2,3),
+(3,1,5,4),
+(4,2,3,2),
+(5,2,4,2),
+(6,2,1,3),
+(7,3,1,1),
+(8,3,2,5),
+(9,2,4,3);
+
