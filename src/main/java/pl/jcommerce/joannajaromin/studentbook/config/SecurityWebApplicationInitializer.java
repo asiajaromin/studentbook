778c17cd8c18cb0df8d3254141e000589aacf46e
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-04 10:06:03 +0200

    security deleted

--- src/main/java/pl/jcommerce/joannajaromin/studentbook/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package pl.jcommerce.joannajaromin.studentbook.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
