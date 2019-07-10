53eab5d33e7e149148c5c4bc5308b383cbdc078a
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-10 10:08:38 +0200

    deleted method

--- src/main/java/pl/jcommerce/joannajaromin/studentbook/service/HomeworkService.java
+++ src/main/java/pl/jcommerce/joannajaromin/studentbook/service/HomeworkService.java
@@ -12,7 +12,6 @@ public interface HomeworkService {
 
     HomeworkDtoWithoutFile findById(int homeworkId);
 
-    MultipartFile getFile(int homeworkId);
-
     ByteArrayResource downloadFile(int fileId);
+
 }
