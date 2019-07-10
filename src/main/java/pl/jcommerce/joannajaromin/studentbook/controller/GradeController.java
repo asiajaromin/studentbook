a5b5987537a9dfdad1563c169c21f25fe03fc856
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-10 16:14:37 +0200

    exceptions

--- src/main/java/pl/jcommerce/joannajaromin/studentbook/controller/GradeController.java
+++ src/main/java/pl/jcommerce/joannajaromin/studentbook/controller/GradeController.java
@@ -1,7 +1,14 @@
 package pl.jcommerce.joannajaromin.studentbook.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import pl.jcommerce.joannajaromin.studentbook.dto.GradeDto;
 import pl.jcommerce.joannajaromin.studentbook.dto.SaveGradeDto;
 import pl.jcommerce.joannajaromin.studentbook.service.GradeService;
@@ -10,17 +17,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class GradeController {
 
     private final GradeService gradeService;
 
     @GetMapping("/grades")
-    public List<GradeDto> findAll(){
+    public List<GradeDto> findAll() {
+        log.info("Szukam ocen");
         return gradeService.findAll();
     }
 
     @GetMapping("/grades/{gradeId}")
     public GradeDto getGrade(@PathVariable int gradeId){
+        log.info("Szukam oceny o id {}", gradeId);
         return gradeService.findById(gradeId);
     }
 
