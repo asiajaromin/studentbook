c66a9cf4f71a25681f945c490f76b543d6f29327
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-25 11:41:45 +0200

    added groovy tests and handled null in UserService

--- src/main/java/pl/jcommerce/joannajaromin/studentbook/service/UserService.java
+++ src/main/java/pl/jcommerce/joannajaromin/studentbook/service/UserService.java
@@ -1,22 +1,76 @@
 package pl.jcommerce.joannajaromin.studentbook.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pl.jcommerce.joannajaromin.studentbook.entity.User;
 import pl.jcommerce.joannajaromin.studentbook.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
-
-    protected final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails userDetails = new MyUserDetails(this, username);
-        return userDetails;
+        User user = userRepository.findByUsername(username);
+        return Optional.ofNullable(user)
+                .map(user1 -> new MyUserDetails(user1))
+                .orElseThrow(()-> new UsernameNotFoundException("Brak użytkownika o nazwie: " + username));
     }
 
+    private static class MyUserDetails implements UserDetails {
+
+        private final User user;
+
+        public MyUserDetails(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            String authority = user.getAuthority();
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority);
+            Collection<GrantedAuthority> authorities = Arrays.asList(grantedAuthority);
+            return authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return user.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return user.getUsername();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
 }
---
+++ src/test/groovy/CustomUserServiceTest.groovy
@@ -0,0 +1,50 @@
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import pl.jcommerce.joannajaromin.studentbook.entity.User
+import pl.jcommerce.joannajaromin.studentbook.repository.UserRepository
+import pl.jcommerce.joannajaromin.studentbook.service.UserService
+import spock.lang.Specification
+
+class CustomUserServiceTest extends Specification {
+
+    private User user
+    private int USER_ID = 12
+    private String USERNAME = "Best User"
+    private String INCORRECT_USERNAME = "Bad user"
+    private String PASSWORD = "MySecretPassword"
+    private String STUDENT_ROLE = "ROLE_STUDENT"
+    private UserService userService
+
+    def "UserService should return UserDetails based on username"() {
+        given:
+        def userRepository = Mock(UserRepository)
+        user = new User(USER_ID, USERNAME, PASSWORD, STUDENT_ROLE)
+        userService = new UserService(userRepository)
+        userRepository.findByUsername(USERNAME) >> user
+
+        when:
+        UserDetails userFromService = userService.loadUserByUsername(USERNAME)
+
+        then:
+        userFromService.username == user.username
+        userFromService.username == user.username
+        userFromService.password == user.password
+        userFromService.authorities.size() == 1
+        userFromService.authorities[0].authority == user.authority
+    }
+
+    def "UserService should throw exception when user doesn't exist"() {
+        given:
+        def userRepository = Mock(UserRepository)
+        userService = new UserService(userRepository)
+
+        when:
+        userRepository.findByUsername(INCORRECT_USERNAME) >> null
+        UserDetails userFromService = userService.loadUserByUsername(INCORRECT_USERNAME)
+
+        then:
+        userFromService == null
+        UsernameNotFoundException exception = thrown()
+        exception.message == 'Brak użytkownika o nazwie: ' + INCORRECT_USERNAME
+    }
+}
