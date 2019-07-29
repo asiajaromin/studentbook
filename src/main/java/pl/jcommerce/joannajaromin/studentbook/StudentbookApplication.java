d7293e0315defea22efa2b01037eb295caac39a8
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-29 13:24:03 +0200

    enabled async

--- src/main/java/pl/jcommerce/joannajaromin/studentbook/StudentbookApplication.java
+++ src/main/java/pl/jcommerce/joannajaromin/studentbook/StudentbookApplication.java
@@ -2,12 +2,28 @@ package pl.jcommerce.joannajaromin.studentbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class StudentbookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentbookApplication.class, args);
     }
 
+    @Bean(name = "asyncExector")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
 }
