a102eb90dad8fa77078145154e52c316c6bf2bbc
asiajaromin <joanna.jaromin@jcommerce.pl>
2019-07-30 12:38:45 +0200

    not working test for mailService

--- src/test/java/pl/jcommerce/joannajaromin/studentbook/service/MailServiceTest.java
+++ src/test/java/pl/jcommerce/joannajaromin/studentbook/service/MailServiceTest.java
@@ -1,6 +1,7 @@
 package pl.jcommerce.joannajaromin.studentbook.service;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,10 +14,12 @@ import pl.jcommerce.joannajaromin.studentbook.repository.StudentRepository;
 import pl.jcommerce.joannajaromin.studentbook.repository.SubjectRepository;
 
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class MailServiceTest {
 
@@ -49,22 +52,25 @@ public class MailServiceTest {
         mailSender = mock(JavaMailSender.class);
         subjectRepository = mock(SubjectRepository.class);
         studentRepository = mock(StudentRepository.class);
+        htmlTemplateEngine = mock(TemplateEngine.class);
         mailService = new MailServiceImpl(mailSender, templateEngine);
         gradeDto = new GradeDto(GRADE_ID, STUDENT_ID, SUBJECT_ID, GRADE);
         emailData = new EmailData(SUBJECT_NAME,GRADE,student);
-        htmlTemplateEngine = mock(TemplateEngine.class);
     }
 
+    @Ignore
     @Test
     public void canSendEmail() throws MessagingException {
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessage mimeMessage = new MimeMessage((Session)null);
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
-        messageHelper.setSubject("Wystawiono nową ocenę z przedmiotu: " + emailData.getSubjectName());
+        messageHelper.setSubject(EXPECTED_EMAIL_SUBJECT);
         messageHelper.setTo(emailData.getStudentEmail());
         Context context = new Context();
         context.setVariable("emailData", emailData);
-        String htmlContent = this.htmlTemplateEngine.process("email-template",context);
-        messageHelper.setText(htmlContent,true);
+        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+//        problem in the following line. Null pointer exception
+//        http://unittesting1.blogspot.com/2016/04/mockito-mocking-templateengine-throws.html
+        when(htmlTemplateEngine.process("email-template",context)).thenReturn(" ");
         mailService.sendEmailToStudentAboutNewGrade(emailData);
         verify(mailSender).send(mimeMessage);
     }
